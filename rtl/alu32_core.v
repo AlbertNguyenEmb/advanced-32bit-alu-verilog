6728b1962e6567740263ecf370c1b83c0d4eb0ce
Nhat Nguyen <albert.nguyentechemb@gmail.com>
2026-06-07 23:17:58 +0700

    Add shift operations to ALU core

--- rtl/alu32_core.v
+++ rtl/alu32_core.v
@@ -3,29 +3,38 @@ module alu32_core(
     input wire [31:0] A,
     input wire [31:0] B,
     input wire [5:0] opcode,
+    input wire [4:0] shamt,
     output reg [31:0] Result,
     output wire Zero,
     output wire Negative,
     output wire Carry,
     output wire Overflow
+    
 );
     //Opcode definitions
+    //Basic arithmetic and logic opcodes
     localparam OP_ADD = 6'b000000;
     localparam OP_SUB = 6'b000001;
     localparam OP_AND = 6'b000010;
     localparam OP_OR = 6'b000011;
     localparam OP_XOR = 6'b000100;
     localparam OP_NOT = 6'b000101;
-    //Comapare
+    //Comaparision opcodes
     localparam EQ = 6'b000110;
     localparam LT_UNSIGNED = 6'b000111;
     localparam GT_UNSIGNED = 6'b001000;
     localparam LT_SIGNED = 6'b001001;
     localparam GT_SIGNED = 6'b001010;
-
+    //Shift opcodes
+    localparam OP_SLL = 6'b001011;
+    localparam OP_SRL = 6'b001100;
+    localparam OP_SRA = 6'b001101;
     //Create bit carry
     wire [32:0] add_result;
+    wire [32:0] sub_result;
+
     assign add_result = {1'b0, A} + {1'b0, B};
+    assign sub_ext = {1'b0, A} - {1'b0, B};
     assign Carry = (opcode == OP_ADD) ? add_result[32] : 1'b0;
     //Create bit overflow
     assign Overflow = ((opcode == OP_ADD)
@@ -33,17 +42,23 @@ module alu32_core(
                             (A[31] && B[31] && ~Result[31])));
     always @(*) begin
         case (opcode)
+            //Arithmetic and logic
             OP_ADD: Result = A + B;
             OP_SUB: Result = A - B;
             OP_AND: Result = A & B;
             OP_OR: Result = A | B;
             OP_XOR: Result = A ^ B;
             OP_NOT: Result = ~A;
+            //Comparision
             EQ: Result = (A == B) ? 32'b1 : 32'b0;
             LT_UNSIGNED: Result = (A < B) ? 32'b1 : 32'b0;
             GT_UNSIGNED: Result = (A > B) ? 32'b1 : 32'b0;
             LT_SIGNED: Result = ($signed(A) < $signed(B)) ? 32'b1 : 32'b0;
             GT_SIGNED: Result = ($signed(A) > $signed(B)) ? 32'b1 : 32'b0;
+            //Shift
+            OP_SLL: Result = A << shamt;
+            OP_SRL: Result = A >> shamt;
+            OP_SRA: Result = $signed(A) >>> shamt;
             default: Result = 32'h0000_0000;
         endcase
     end
--- tb/alu32_core_tb.v
+++ tb/alu32_core_tb.v
@@ -4,6 +4,7 @@ module alu32_core_tb();
 reg [31:0] A;
 reg [31:0] B;
 reg [5:0] opcode;
+reg [4:0] shamt;
 wire [31:0] Result;
 wire Zero;
 wire Negative;
@@ -14,6 +15,7 @@ alu32_core dut(
     .opcode(opcode),
     .Result(Result),
     .Zero(Zero),
+    .shamt(shamt),
     .Negative(Negative)
 );
 
@@ -30,6 +32,19 @@ begin
 end
 endtask
 
+task run_test_shift;
+    input [5:0] op;
+    input [31:0] a;
+    input [4:0] shift_amt;
+begin
+    opcode = op;
+    A = a;
+    shamt = shift_amt;
+    #10;
+    $display("Time = %0t, Opcode = %0b, A = %0h, shamt = %d, Result = %0d", 
+            $time, op, a, shamt, Result);
+end
+endtask
     //Opcode definitions
     localparam OP_ADD = 6'b000000;
     localparam OP_SUB = 6'b000001;
@@ -45,17 +60,24 @@ endtask
     localparam GT_SIGNED = 6'b001010;
 
     initial begin
+        /*
+        //Test Arithmetic and logic
         run_test(6'b000000, 10, 20); // ADD
         run_test(6'b000001, 50, 15); // SUB
         run_test(6'b000010, 32'h0000_0000,  32'h0000_0001);  // AND
         run_test(6'b000011, 32'h1111_1111,  32'h0000_0000);  // OR
         run_test(6'b000100, 32'h1100_1100,  32'h0011_0011);  // XOR
-        run_test(6'b000101, 32'h1100_1100, 32'h0000_0000);  // NOT 
-        /*run_test(6'b000110, 32'h1111_1111, 32'h1111_1111); // 1
+        run_test(6'b000101, 32'h1100_1100, 32'h0000_0000);  // NOT
+        //Test comparision
+        run_test(6'b000110, 32'h1111_1111, 32'h1111_1111); // 1
         run_test(6'b000111, 32'h1111_1111, 32'h1111_1110); //0
         run_test(6'b001000, 32'h1111_1111, 32'h1111_1110); // 1
         run_test(6'b001001, 32'hFFFF_FFFF, 32'h1111_1111); //1
         run_test(6'b001010, 32'hFFFF_FFFF, 32'h1111_1111); //0 */
+        //Test shift
+        run_test_shift(6'b001011, 32'h1111_1111, 5'b01000); //SLL 2bit -> 1111_1100
+        run_test_shift(6'b001100, 32'h1111_1111, 5'b01000); //SRL 2bit -> 0011_1111
+        run_test_shift(6'b001101, 32'h1100_0000, 5'b01000); //SRA 2bit -> 1110_0000
     end
     
 endmodule
